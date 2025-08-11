f7dcd13f118c90a4279541dfeb94795c766954c2
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-08-12 01:59:36 +0800

    responsive meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -15,6 +15,12 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "SoundWave | Earcommerce",
   description: "We offer the best portable audio devices in town",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  // You can add other properties like minimumScale, maximumScale, userScalable
 };
 
 export default function RootLayout({

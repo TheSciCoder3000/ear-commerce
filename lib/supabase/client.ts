4cc0b4f7c12eefa35e18f478816a845fefee60c6
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-08-16 22:50:01 +0800

    updated create client util function for api endpoint

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,8 +1,16 @@
 import { createBrowserClient } from "@supabase/ssr";
 
-export function createClient() {
+export function createClient(token?: string) {
+  const tokenize = !token
+    ? {}
+    : {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    { global: tokenize }
   );
 }

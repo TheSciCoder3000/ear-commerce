6a18026cf540e01876732df918034ff411027c2a
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-08-31 20:25:03 +0800

    cart page

---
+++ app/cart/page.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import Image from "@/components/Image";
+import { useCart } from "@/store/hooks";
+import React from "react";
+
+const CartPage = () => {
+  const cart = useCart();
+  const fees = 1;
+
+  return (
+    <div className="mt-30 mb-20 min-h-[70vh]">
+      <div className="max-w-[80rem] mx-auto px-5">
+        <h1 className="text-3xl mb-4">Cart</h1>
+        <hr className="bg-gray-100 h-[2px] mb-5" />
+
+        <div className="flex flex-col lg:flex-row gap-10">
+          <div className="flex-1 flex flex-col gap-5">
+            {cart.map((item) => (
+              <div key={item.id} className="flex gap-5 w-full">
+                <Image
+                  className="w-[7.5rem] aspect-square"
+                  src={item.image_paths[0]}
+                  alt={item.id}
+                />
+                <div className="w-full flex flex-col justify-between pb-2">
+                  <div>
+                    <h3 className="text-lg">{item.name}</h3>
+                    <p className="mt-1 text-gray-500 text-sm">
+                      {item.category.name}
+                    </p>
+                  </div>
+
+                  <div className="flex justify-between font-semibold">
+                    <h3>$ {item.price}</h3>
+                    <h3>x {item.count}</h3>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="p-5 w-full lg:w-[20rem] rounded-xl shadow-md h-full">
+            <h2 className="text-xl mb-5">Summary</h2>
+            <div className="grid grid-cols-3 text-gray-500 gap-y-2">
+              <h3 className="col-span-2">Sub Total:</h3>
+              <h3>
+                ${" "}
+                {cart.reduce(
+                  (state, item) => state + item.price * item.count,
+                  0
+                )}
+              </h3>
+
+              <h3 className="col-span-2">Fees:</h3>
+              <h3>$ {fees}</h3>
+
+              <hr className="col-span-3 bg-gray-300 h-[2px] mt-3" />
+
+              <h3 className="col-span-2 font-bold">Total:</h3>
+              <h3 className="font-bold">
+                ${" "}
+                {cart.reduce(
+                  (state, item) => state + item.price * item.count,
+                  0
+                ) + fees}
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartPage;
